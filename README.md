d8b16f27dc59b36d55ffcc27945bddd2224212ef
Apratim Bajpai <apri4u@gmail.com>
2023-12-19 21:09:26 -0500

    Dynami URL

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# face_recognition_flask
+# Flask fundamentals with Face Recognition app
+
+
+## Redirect and url_to
+
+This can be use to build dynamic URLs
+
+``` python
+@app.route("/result/<int:score>")
+def result(score):
+    status = "success" if score > 60 else "fail"
+    return redirect(url_for(status, score=score))
+
+@app.route("/pass/<int:score>")
+def success(score):
+    return "The person has passed!"
+
+@app.route("/fail/<int:score>")
+def fail(score):
+    return "The person has failed"
+```
+
+Depending upon the score, the URL will change
+```
+http://127.0.0.1:5000/result/40 -> http://127.0.0.1:5000/fail/40
+"The person has failed"
+
+http://127.0.0.1:5000/result/100 -> http://127.0.0.1:5000/pass/100
+"The person has passed!"
+```
--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask
-# WSGI Application
+### Building Url-Dynamically.
+### Variable Rules And URL Building
+
+from flask import Flask, redirect, url_for
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def welcome():
-    return "hello, neesan, world!"
+def hello():
+    return "Hello, world!"
 
-@app.route('/members')
-def goodbye():
-    return "Bye, bye!"
+@app.route("/result/<int:score>")
+def result(score):
+    status = "success" if score > 60 else "fail"
+    return redirect(url_for(status, score=score))
 
+@app.route("/pass/<int:score>")
+def success(score):
+    return "The person has passed!"
+
+@app.route("/fail/<int:score>")
+def fail(score):
+    return "The person has failed"
+    
 if __name__ == "__main__":
     app.run(debug=True)
